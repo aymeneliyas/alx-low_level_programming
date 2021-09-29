47785e638a4986b85b4c9aef414a0c97283bbc2f
Aymen eliyas <aymeneliyas8@gmail.com>
2021-09-29 09:18:39 -0700

    100

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,32 +1,32 @@
-#include "function_pointers.h"
-
-
-
-/*
-*Intresting note: functions are a set of instruction and
-*function pointers are pointers to the begning of this instruction
-*so if we could get the first instrucions adress inmemory
-*we can see what it is doing by printing the adresses
-*/
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - print opcodes of its own main function
- * @argc: number of arguments
- * @argv: vector of arguments
- * Return: 0 if no errors
+ * main - program that prints the opcodes of its own main function
+ * @argc: count of arguments
+ * @argv: array of arguments
+ * Return: int
  */
-int main(int argc, char *argv[])
+
+int main(int argc, char **argv)
 {
-	register int i, num;
-	char *ptr = (char *)main;
+	int i, bytes;
+	char *addr;
 
 	if (argc != 2)
-		printf("Error\n"), exit(1);
-	num = atoi(argv[1]);
-	if (num < 0)
-		printf("Error\n"), exit(2);
-	for (i = 0; i < num - 1; i++)
-		printf("%02hhx ", ptr[i]);
-	printf("%02hhx\n", ptr[i]);
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	addr = (char *)main;
+	for (i = 0; i < bytes - 1; i++)
+		printf("%02hhx ", addr[i]);
+	printf("%02hhx\n", addr[i]);
 	return (0);
 }
