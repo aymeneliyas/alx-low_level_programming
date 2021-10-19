3910d83b8b54c361fb55b18ab7aadfffc0741f01
Aymen eliyas <aymeneliyas8@gmail.com>
2021-10-19 09:50:51 -0700

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,34 +1,90 @@
 #include "main.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 /**
- * create_file - creates a file and fills it with text
+ * _strlen - finds the length of a string
+ * @str: pointer to the string
+ *
+ * Return: length of the string
+ */
+size_t _strlen(char *str)
+{
+	size_t i;
+
+	for (i = 0; str[i]; i++)
+		;
+#include "main.h"
+#include <unistd.h>
+#include "holberton.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include "holberton.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include "holberton.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include "holberton.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include "holberton.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include "holberton.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include "holberton.h"
+#include "holberton.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include "holberton.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/**
+ * _strlen - finds the length of a string
+ * @str: pointer to the string
+ *
+ * Return: length of the string
+ */
+size_t _strlen(char *str)
+{
+	size_t i;
+
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
+}
+
+/**
+ * create_file - creates a file.
  * @filename: name of the file to create
- * @text_content: text to write in the file
+ * @text_content: NULL terminated string to write to the file
  *
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, x, y = 0;
+	int fd;
+	ssize_t len = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
-
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (fd < 0)
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
 		return (-1);
-
-	if (text_content)
-	{
-		while (text_content[y])
-			y++;
-		x = write(fd, text_content, y);
-		if (x != y)
-			return (-1);
-	}
-
+	if (text_content != NULL)
+		len = write(fd, text_content, _strlen(text_content));
 	close(fd);
-
+	if (len == -1)
+		return (-1);
 	return (1);
 }
 
